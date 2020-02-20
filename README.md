0146f78dc8ec9733ddcb0628fe2b84a414fbf583
antoinezambelli <antoine.zambelli@gmail.com>
2020-02-19 18:50:35 -0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # sc2downtime
-Downtime calculator for buildings in SC2
+Baseline script for computing structure downtime from SC2 replays.
+
+## Description
+
+This is a hacked-together bit of code that computes the time your structures spent NOT producing units like they should have been doing. Note that it computes these numbers for all structures in a "type" (ie, all `Factory`, all `OrbitalCommand`) - it does not attribute a unit to a specific building (ie, the second `Factory` you build that game).
+
+## Incomplete
+
+There are some hardcoded `dict`s and `list`s in this code that I only populated as far as they were useful for the replay I debugged this with. They need to be populated with the remaining units/buildings in the game.
+
+## Unit Attribution
+
+Attributing a unit to a specific building is harder than it seems. The only way to do it would be using the `location` of the `UnitBornEvent`, and selecting the closest building there.
+
+The script does this already, but it will mis-attribute if you build structures very close to one another.
+
+Example: you build all your `Factory` North-South stacked on top of each other, and pump out `Thors`. There's a good chance the `Thor` coming out of the Northern-most `Factory` will be attributed to the 2nd-most Northern `Factory`.
+
+I haven't computed any metrics on how often this occurs, or how far apart buildings need to be. The best I hacked was to attribute to the closest `parent` building (so the Thor won't go to the nearby `Barracks`).
+
+## Usage
+
+Simply update the path to the replay file, or comment that line and uncomment the `tkinter` block.
+
+Update the `PLAYER` variable to your handle. Running the script should then work fine.
+
+The script requires the `sc2reader` and `pandas` libraries, both can be installed with `pip install`.
+
+## Maintenance
+
+Honestly, I don't plan on maintaining this code or repo - just wanted a way to hand it off to anyone who's interested. Feel free to fork it, clone it, etc :)
+
+## Bonus Level
+
+The obvious next improvement would be to create a timeseries dataframe, appending each new `UnitBornEvent` - instead of just adding the times within the loop.
+
+GLHF!
---
+++ baseline.py
@@ -0,0 +1,133 @@
+import sc2reader
+import pandas as pd
+
+## tkinter file dialog version.
+# import os
+# from tkinter.filedialog import askopenfilename
+
+
+# root_fp = os.environ['USERPROFILE']  # To get user name.
+# sc2_root_fp = os.path.join(root_fp, 'Documents/StarCraft II/Accounts')  # Drill-down.
+
+# replay_fp = askopenfilename(initialdir=sc2_root_fp)  # Open dialog to select replay.
+
+# replay = sc2reader.load_replay(replay_fp)  # Load it.
+
+replay = sc2reader.load_replay(
+    '/path/to/Documents/StarCraft II/Accounts/xxxx/yyyy/Replays/Multiplayer/the_replay.SC2Replay'
+)
+
+NON_PROD_BUILDINGS = [
+    'Armory',
+    'Bunker',
+    'EngineeringBay',
+    'Refinery',
+    'SupplyDepot',
+    'TechLab',
+    'Turret',
+]  # list of non-production buildings to exclude - has to be hardcoded.
+
+ADD_ONS = [
+    'TechLab'
+]  # list add-on buildings to account for prod time - has to be hardcoded.
+
+UNITS = {
+    'Hellion': {'time': 21, 'parent': 'Factory'},
+    'Marine': {'time': 18, 'parent': 'Barracks'},
+    'SCV': {'time': 12, 'parent': 'Command'},
+    'Thor': {'time': 43, 'parent': 'Factory'}
+}  # dict of units with build times and parent building - has to be hardcoded.
+
+PLAYER = 'Genie'  # Player name, could easily loop over unique player names.
+
+# Get all buildings that belong to player and can produce (hardcoded).
+buildings = {}
+for e in replay.events:
+    if e.name == 'UnitDoneEvent' and e.second > 0:
+        if e.unit.owner.name == PLAYER and \
+           all(ss not in e.unit._type_class.name for ss in NON_PROD_BUILDINGS):
+
+            buildings[e.unit] = {
+                't_0': e.second,  # Building creation time.
+                'id': e.unit_id,  # Unique id for building.
+                't_build': 0,  # Time spent producing (in seconds).
+                'name': e.unit._type_class.name  # Name.
+            }
+
+# Get buildings location - filter for player.
+for e in replay.events:
+    if e.name == 'UnitInitEvent' and e.second > 0:
+        if e.unit in buildings:
+            buildings[e.unit]['loc'] = e.location
+
+# Track add-on construction like it was a unit.
+for e in replay.events:
+    if e.name == 'UnitInitEvent' and e.second > 0:
+        if e.unit.owner.name == PLAYER and \
+           any(ss in e.unit._type_class.name for ss in ADD_ONS):
+
+            # Match add-on to nearest building.
+            best_match = min(
+                buildings.items(),
+                key=lambda x: (x[1]['loc'][0] - e.location[0]) ** 2 + (x[1]['loc'][1] - e.location[1]) ** 2
+            )[0]
+
+            # Add build time as per UnitDoneEvent of that unit.
+            delta_t = next(
+                ee.second for ee in replay.events if ee.name == 'UnitDoneEvent' and ee.unit == e.unit
+            ) - e.second
+            buildings[best_match]['t_build'] = buildings[best_match]['t_build'] + delta_t
+
+# Track unit creation - ignore MULEs.
+for e in replay.events:
+    if e.name == 'UnitBornEvent' and e.second > 0:
+        if e.unit.owner.name == PLAYER and \
+           any(ss in e.unit._type_class.name for ss in UNITS):
+
+            # Match unit to nearest building.
+            unit_parent_buildings = next(
+                v['parent'] for (k, v) in UNITS.items() if k in e.unit._type_class.name
+            )  # hardcoded parent building for a unit.
+
+            best_match = sorted(
+                buildings.items(),
+                key=lambda x: (x[1]['loc'][0] - e.location[0]) ** 2 + (x[1]['loc'][1] - e.location[1]) ** 2
+            )  # sorted list of buildings by distance.
+            best_match = next(
+                k for k, v in best_match if unit_parent_buildings in v['name']
+            )  # item selected based on closest parent.
+
+            # Add build time as per hardcoded time of that unit.
+            buildings[best_match]['t_build'] = buildings[best_match]['t_build'] + next(
+                v['time'] for (k, v) in UNITS.items() if k in e.unit._type_class.name
+            )
+
+# Get game end time in seconds - from first player leave event.
+for e in replay.events:
+    if e.name == 'PlayerLeaveEvent' and e.second > 0:
+        t_final = e.second
+        break
+
+# Populate prod_time info.
+for b in buildings:
+    buildings[b]['prod_time'] = (t_final - buildings[b]['t_0']) - buildings[b]['t_build']
+    buildings[b]['prod_time_perc'] = buildings[b]['prod_time'] / (t_final - buildings[b]['t_0'])
+
+print(buildings)
+print()
+
+df = pd.DataFrame.from_dict(buildings, orient='index')[['t_0', 'prod_time', 'prod_time_perc', 'name']]
+df['available_time'] = t_final - df['t_0']
+df = df.groupby('name').agg({'prod_time': 'sum', 'available_time': 'sum'})
+df['prod_time_perc'] = df['prod_time'] / df['available_time']
+df = df.sort_values(by='prod_time_perc')
+df['prod_time_perc'] = df['prod_time_perc'].apply(lambda x: '{:.2f}%'.format(100 * x))
+
+print('Sums of time across building types.')
+print(df)
+
+# Obvious improvement (OI):
+# 1) don't just add build times, store them with their init time so
+# we can build a graph over time: ie, after 7:00 you lost focus and stopped using Factory.
+# For this, likely want to store events in a timeseries df with a 'parent' column.
+# After that, we can window(), rolling() and cumsum() our little hearts out.
